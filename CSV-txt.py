65b938d71a0f5c311c2df73c365f745d8b85758d
VictorLier <victor@lier.dk>
2024-06-07 11:59:10 +0200

    CSV to txt added

---
+++ CSV-txt.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+dataLocation = '1.3'
+dataName = 'Residuals.csv'
+
+# Load the data
+collumnNames = np.loadtxt(dataLocation + '/' + dataName, delimiter=',', max_rows=1, dtype=str)
+collumnNames = np.char.strip(collumnNames, '"')
+collumnNames = [col.split(':')[0] for col in collumnNames]
+print(collumnNames)
+data = np.loadtxt(dataLocation + '/' + dataName, delimiter=',', skiprows=1)
+
+for i, name in enumerate(collumnNames):
+    printData = data[:, [0, i]]
+    np.savetxt(dataLocation + '/' + name + '.txt', printData)
